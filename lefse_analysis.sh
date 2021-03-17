721cc7e311adfa6669acacdfc38aa91d5083d3b6
Ben Lorentz <bjl34716@uga.edu>
2021-03-17 11:05:08 -0400

    need to move around directories and then copy the results

--- lefse_analysis.sh
+++ lefse_analysis.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
-
-lefse_files=`ls combos/*lefse_formatted.txt`
+cd combos
+lefse_files=`ls *lefse_formatted.txt`
 for eachfile in $lefse_files
 do
     format_input.py $eachfile lefse_formatted.in -c 2 -u 1
     run_lefse.py lefse_formatted.in lefse_result.res 
-    plot_res.py lefse_result.res result/"${eachfile::-4}"_res.png --dpi 100 
-    plot_res.py lefse_result.res result/"${eachfile::-4}"_pdf_r.png --dpi 72
-    plot_cladogram.py lefse_result.res result/"${eachfile::-4}"_cladogram.png --format png --dpi 200
-    plot_cladogram.py lefse_result.res result/"${eachfile::-4}"_pdf_qual_clad.png --format png --dpi 72
-    cp lefse_result.res result/"${eachfile::-4}"_result.res
+    plot_res.py lefse_result.res "${eachfile::-4}"_res.png --dpi 100 
+    plot_res.py lefse_result.res "${eachfile::-4}"_pdf_r.png --dpi 72
+    plot_cladogram.py lefse_result.res "${eachfile::-4}"_cladogram.png --format png --dpi 200
+    plot_cladogram.py lefse_result.res "${eachfile::-4}"_pdf_qual_clad.png --format png --dpi 72
+    cp lefse_result.res "${eachfile::-4}"_result.res
+    cp "${eachfile::-4}"_res.png ../result
+    cp "${eachfile::-4}"_pdf_r.png ../result
+    cp "${eachfile::-4}"_cladogram.png ../result
+    cp "${eachfile::-4}"_pdf_qual_clad.png ../result
 done
