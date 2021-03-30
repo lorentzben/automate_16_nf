d9bf9f83ba0c53e6412b5f59843d2f49ba248ce1
Ben Lorentz <bjl34716@uga.edu>
2021-03-30 08:54:54 -0400

    add version # to jamba

--- qiime_to_lefse.R
+++ qiime_to_lefse.R
@@ -3,7 +3,7 @@ if(!require(dplyr)){install.packages("dplyr",repos="http://cran.us.r-project.org
 if(!require(tibble)) {install.packages("tibble",repos="http://cran.us.r-project.org")}
 if(!require(qiime2R)) {devtools::install_github("jbisanz/qiime2R")} # current version is 0.99.20
 if(!require(phyloseq)) {install.packages("phyloseq",repos="http://cran.us.r-project.org")}
-if(!require(jamba)){remotes::install_github("jmw86069/jamba")}
+if(!require(jamba)){remotes::install_github("jmw86069/jamba@0.0.6.900")}
 
 
 args = commandArgs(trailingOnly=TRUE)
