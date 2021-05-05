c99c65d570993de3cfec3196a0a254eb38a9ecbd
Ben Lorentz <bjl34716@uga.edu>
2021-05-05 11:47:26 -0400

    added lib calls instead

--- qiime_to_lefse.R
+++ qiime_to_lefse.R
@@ -6,7 +6,11 @@
 #if(!require(qiime2R)) {devtools::install_github("jbisanz/qiime2R")} # current version is 0.99.20
 #if(!require(phyloseq)) {install.packages("phyloseq",repos="http://cran.us.r-project.org")}
 #if(!require(jamba)){remotes::install_github("jmw86069/jamba@0.0.6.900")}
-
+library(dplyr)
+library(tibble)
+library(qiime2R)
+library(phyloseq)
+library(jamba)
 
 args = commandArgs(trailingOnly=TRUE)
 if (length(args)==0) {
