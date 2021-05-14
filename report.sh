0a3c931d293c42544505003a1cdca5fc2d26a10e
Ben Lorentz <bjl34716@uga.edu>
2021-05-14 10:47:33 -0400

    whoops, I think i fixed report.sh now

--- report.sh
+++ report.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env
-eval "$(conda shell.bash hook)"
-conda activate work/conda/r_env*
+#!/usr/bin/env bash
+cp init_and_refresh.R $(cat out.txt)
 cp report.Rmd $(cat out.txt)
 cp make_report.sh $(cat out.txt)
 cp order_item_of_interest.csv $(cat out.txt)
 cd $(cat out.txt)
 echo $PWD
+Rscript init_and_refresh.R
 bash make_report.sh
