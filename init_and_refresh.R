4b5c5ed228e13dc154bb54ea574a704e085937ff
Benjamin Lorentz <bjl34716@uga.edu>
2021-06-18 10:12:55 -0400

    change the order'

--- init_and_refresh.R
+++ init_and_refresh.R
@@ -3,11 +3,10 @@ if(!require(renv)) {install.packages("renv",repos="http://cran.us.r-project.org"
 renv::init()
 install.packages("BiocManager")
 BiocManager::install(version = "3.13")
-renv::restore()
 if(!require(dplyr)){install.packages("dplyr",repos="http://cran.us.r-project.org")}
 if(!require(tibble)) {install.packages("tibble",repos="http://cran.us.r-project.org")}
 if(!require(qiime2R)) {devtools::install_github("jbisanz/qiime2R")} # current version is 0.99.20
 if(!require(phyloseq)) {install.packages("phyloseq",repos="http://cran.us.r-project.org")}
 if(!require(jamba)){remotes::install_github("jmw86069/jamba@0.0.6.900")}
-
+renv::restore()
 q()
