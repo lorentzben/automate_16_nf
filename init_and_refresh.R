6b0fd49980a668332cab312dd3bd9124d19946a1
Benjamin Lorentz <bjl34716@uga.edu>
2021-10-20 11:39:45 -0400

    moved rhdf5 before dplyr

--- init_and_refresh.R
+++ init_and_refresh.R
@@ -3,8 +3,8 @@ if(!require(renv)) {install.packages("renv",repos="http://cran.us.r-project.org"
 renv::init()
 install.packages("BiocManager")
 BiocManager::install(version = "3.13")
-if(!require(dplyr)){renv::install("dplyr")}
 if(!require(rhdf5)){renv::install("rhdf5")}
+if(!require(dplyr)){renv::install("dplyr")}
 if(!require(tibble)) {renv::install("tibble")}
 if(!require(qiime2R)) {renv::install("jbisanz/qiime2R@d1ad96657ada993cf6c2841b29113a4f635c6b56")} # current version is 0.99.20
 if(!require(phyloseq)) {BiocManager::install("phyloseq", version="3.13")}
