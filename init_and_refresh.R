1821776b44d0d121f9b5d79b92d04a23eeff425b
Benjamin Lorentz <bjl34716@uga.edu>
2021-10-20 16:02:26 -0400

    move around

--- init_and_refresh.R
+++ init_and_refresh.R
@@ -5,12 +5,13 @@ install.packages("BiocManager")
 BiocManager::install(version = "3.13")
 if(!require(rhdf5)){renv::install("bioc::rhdf5")}
 if(!require(Biostrings)){renv::install("bioc::Biostrings")}
+if(!require(Biobase)){renv::install("bioc::Biobase")}
 if(!require(dplyr)){renv::install("dplyr")}
 if(!require(tibble)) {renv::install("tibble")}
 if(!require(phyloseq)) {BiocManager::install("phyloseq", version="3.13")}
 if(!require(qiime2R)) {renv::install("jbisanz/qiime2R@d1ad96657ada993cf6c2841b29113a4f635c6b56")} # current version is 0.99.20
 if(!require(jamba)){renv::install("jmw86069/jamba@0.0.6.900")}
-if(!require(Biobase)){renv::install("bioc::Biobase")}
+
 renv::restore()
 q()
 
