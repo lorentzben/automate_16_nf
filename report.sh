0415ed942592e676026ca98bb4caf6f3ccbe2943
Benjamin Lorentz <bjl34716@uga.edu>
2021-11-22 16:55:33 -0500

    edit report.sh:

--- report.sh
+++ report.sh
@@ -5,5 +5,5 @@ cp make_report.sh $(cat out.txt)
 cp order_item_of_interest.csv $(cat out.txt)
 cd $(cat out.txt)
 echo $PWD
-Rscript init_and_refresh.R
+singularity run docker://lorentzb/r_latest Rscript init_and_refresh.R
 bash make_report.sh
