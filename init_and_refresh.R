077bec1986b288de755d223f051c98329e912eda
Benjamin Lorentz <bjl34716@uga.edu>
2021-11-23 11:48:59 -0500

    remove the source

--- init_and_refresh.R
+++ init_and_refresh.R
@@ -12,7 +12,7 @@ if(!require(phyloseq)) BiocManager::install("phyloseq", version="3.13")
 if(!require(jamba)) renv::install("jmw86069/jamba@0.0.6.900")
 if(!require(qiime2R)) renv::install("jbisanz/qiime2R@d1ad96657ada993cf6c2841b29113a4f635c6b56") # current version is 0.99.20
 if(!require(ggdendro)) renv::install("ggdendro@0.1.22", type="source")
-if(!require(pegas)) renv::install("pegas@1.0", type="source")
+if(!require(pegas)) renv::install("pegas@1.0")
 
 #renv::restore()
 q()
