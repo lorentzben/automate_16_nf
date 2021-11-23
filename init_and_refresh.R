11aaa50736401963848c2c80ec9aefae903884ea
Benjamin Lorentz <bjl34716@uga.edu>
2021-11-23 09:26:12 -0500

    ask=false

--- init_and_refresh.R
+++ init_and_refresh.R
@@ -2,7 +2,7 @@
 if(!require(renv)) {install.packages("renv",repos="http://cran.us.r-project.org")}
 renv::init()
 install.packages("BiocManager")
-BiocManager::install(version = "3.13")
+BiocManager::install(version = "3.13", ask=FALSE)
 if(!require(rhdf5)){renv::install("bioc::rhdf5")}
 if(!require(Biostrings)){renv::install("bioc::Biostrings")}
 if(!require(Biobase)){renv::install("bioc::Biobase")}
