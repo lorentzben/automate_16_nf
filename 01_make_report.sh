ed0d4f519a27c0e1ad8de8d8cd8326ff2d728e0f
Benjamin Lorentz <bjl34716@uga.edu>
2022-04-04 14:21:28 -0400

    add new 01

---
+++ 01_make_report.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+echo "I am Here:"
+pwd
+ls
+
+cp ../item_of_interest.csv .
+
+dt=$(date '+%d-%m-%Y_%H.%M.%S');
+
+Rscript -e "rmarkdown::render('01_report.Rmd', output_file='report_$dt.html', output_format='html_document', clean=TRUE)"
+
+Rscript -e "rmarkdown::render('01_report.Rmd', output_file='report_$dt.pdf', output_format='pdf_document', clean=TRUE)"
+
--- report.sh
+++ report.sh
@@ -6,4 +6,5 @@ cp order_item_of_interest.csv $(cat out.txt)
 cd $(cat out.txt)
 echo $PWD
 singularity run docker://lorentzb/r_latest Rscript init_and_refresh.R
+#TODO make a series of copies and calls for report
 bash make_report.sh
