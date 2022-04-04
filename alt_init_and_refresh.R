7c483d8d8a355e9e43af231876e4f4bed475d7f4
Benjamin Lorentz <bjl34716@uga.edu>
2022-04-04 14:09:27 -0400

    add init and refresh alt

---
+++ alt_init_and_refresh.R
@@ -0,0 +1,11 @@
+#! /usr/bin/env Rscript --vanilla
+if(!require(renv)) {install.packages("renv",repos="http://cran.us.r-project.org")}
+renv::isolate()
+renv::isolate()
+renv::settings$snapshot.type("all")
+renv::install("rmarkdown")
+install.packages("BiocManager")
+BiocManager::install(version = "3.12", ask=FALSE)
+BiocManager::repositories(version="3.12")
+BiocManager::install("remotes")
+
