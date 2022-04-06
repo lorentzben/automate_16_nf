412213d824d52ea3a0727b6b110258ab628abf00
Benjamin Lorentz <bjl34716@uga.edu>
2022-04-06 14:47:28 -0400

    changing filename

--- 01_make_report.sh
+++ 01_make_report.sh
@@ -15,7 +15,7 @@ cp ../order_item_of_interest.csv .
 
 dt=$(date '+%d-%m-%Y_%H.%M.%S');
 
-Rscript -e "rmarkdown::render('01_report.Rmd', output_file='report_$dt.html', output_format='html_document', clean=TRUE)"
+Rscript -e "rmarkdown::render('01_report.Rmd', output_file='01_report_$dt.html', output_format='html_document', clean=TRUE)"
 
-Rscript -e "rmarkdown::render('01_report.Rmd', output_file='report_$dt.pdf', output_format='pdf_document', clean=TRUE)"
+Rscript -e "rmarkdown::render('01_report.Rmd', output_file='01_report_$dt.pdf', output_format='pdf_document', clean=TRUE)"
 
