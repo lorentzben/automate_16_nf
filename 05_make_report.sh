0386d5ec15d01c6a857d86de4152d51b2eb005f1
Benjamin Lorentz <bjl34716@uga.edu>
2022-04-08 11:03:53 -0400

    add 05

---
+++ 05_make_report.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+
+echo "I am Here:"
+pwd
+ls
+
+cp -rf /renv_dev/renv .
+cp -rf /renv_dev/renv.lock . 
+
+Rscript -e "renv::init()"
+Rscript -e "renv::install('rmarkdown')"
+
+cp ../item_of_interest.csv .
+cp ../order_item_of_interest.csv .
+
+dt=$(date '+%d-%m-%Y_%H.%M.%S');
+
+Rscript -e "rmarkdown::render('05_report.Rmd', output_file='05_report_$dt.html', output_format='html_document', clean=TRUE)"
+
+Rscript -e "rmarkdown::render('05_report.Rmd', output_file='05_report_$dt.pdf', output_format='pdf_document', clean=TRUE)"
+
