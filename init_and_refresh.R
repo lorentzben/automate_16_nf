09eaaa5dacaaea9efd8d5c5065fec3cc4ede20f2
Benjamin Lorentz <bjl34716@uga.edu>
2022-06-29 16:57:56 -0400

    install staged

--- init_and_refresh.R
+++ init_and_refresh.R
@@ -1,7 +1,9 @@
 #! /usr/bin/env Rscript --vanilla
 #Rscript -e ".libPaths('./r_lib/')"
 .libPaths('./r_lib/')
-renv::settings$use.cache(TRUE)
+Sys.setenv(R_INSTALL_STAGED = "false")
+options(install.opts = "--no-test-load")
+#renv::settings$use.cache(TRUE)
 renv::activate()
 #Rscript -e "renv::init()"
 #Rscript -e "renv::install('rmarkdown')"
