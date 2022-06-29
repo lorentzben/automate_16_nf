1e9ee6c02655c7343276e2178193c533f64d64a4
Benjamin Lorentz <bjl34716@uga.edu>
2022-06-29 16:48:17 -0400

    oops

--- init_and_refresh.R
+++ init_and_refresh.R
@@ -1,9 +1,10 @@
 #! /usr/bin/env Rscript --vanilla
 #Rscript -e ".libPaths('./r_lib/')"
 .libPaths('/renv_dev/r_lib/')
-Rscript -e "renv::activate()"
+renv::settings$use.cache(TRUE)
+renv::activate()
 #Rscript -e "renv::init()"
 #Rscript -e "renv::install('rmarkdown')"
-Rscript -e "renv::restore(library='./renv/library/R-4.1/x86_64-pc-linux-gnu/', lockfile='./renv.lock')"
+renv::restore(library='./renv/library/R-4.1/x86_64-pc-linux-gnu/', lockfile='./renv.lock')
 q()
 
